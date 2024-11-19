f088801afcec14096157bfb1467fd9af69e6fb6a
srnyx <25808801+srnyx@users.noreply.github.com>
2024-11-18 20:07:23 -0500

    Update PlaceholderAPI
    2.11.3 -> 2.11.6

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,5 +12,6 @@ plugins {
 
 setupAnnoyingAPI("4.1.0", "xyz.srnyx", "2.0.0", "A fun little game... when one person dies, everyone dies")
 spigotAPI("1.8.8")
+
 repository(Repository.PLACEHOLDER_API)
-dependencies.compileOnly("me.clip", "placeholderapi", "2.11.3")
+dependencies.compileOnly("me.clip", "placeholderapi", "2.11.6")
